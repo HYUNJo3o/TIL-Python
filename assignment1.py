e512d81adcff97da57d98e203a89071aebfd10dc
HYUNJo3o <sodapop47@cau.ac.kr>
2020-08-30 02:43:00 +0900

    while문 배우기

--- assignment1.py
+++ assignment1.py
@@ -1,17 +1,21 @@
 grade = int(input())
 
-while 0<= grade <=100:
-
-if 90<= grade <=100:
-    print("A")
-elif 80<= grade <90:
-    print("B")
-elif 70<= grade <80:
-    print("C")
-elif 60<= grade <70:
-    print("D")
-elif 0<= grade <60:
-    print("F")
-else:
-    print("잘못된 값을 입력했습니다.")
-    grade = int(input())
+while True:
+  if 90<= grade <=100:
+      print("A")
+      break
+  elif 80<= grade <90:
+      print("B")
+      break
+  elif 70<= grade <80:
+      print("C")
+      break
+  elif 60<= grade <70:
+      print("D")
+      break
+  elif 0<= grade <60:
+      print("F")
+      break
+  else:
+      print("잘못된 값을 입력했습니다.")
+      grade = int(input())
